2b45015fc46d9079acf4f1ac0c51e0d903c253d0
MiguelJMS <miguelmarabay19@gmail.com>
2024-06-18 14:53:13 -0400

    changes in popover menu mobiel

--- src/components/inputs/CustomInput.tsx
+++ src/components/inputs/CustomInput.tsx
@@ -29,7 +29,7 @@ const CustomInput: React.FC<CustomInputProps> = ({ name, label, type = 'text', o
       )}
       <label
         htmlFor={name}
-        className={`absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-btn-blue ${errors[name] ? 'text-red-500' : ''}`}
+        className={`absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-btn-blue ${errors[name] ? 'text-red-500' : ''}`}
       >
         {label}
       </label>
