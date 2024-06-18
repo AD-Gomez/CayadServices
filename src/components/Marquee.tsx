9cb6be5b531fe4c6143ef6cb0e284a8535dbea45
MiguelJMS <miguelmarabay19@gmail.com>
2024-06-18 14:18:26 -0400

    changes width and margin form

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -4,8 +4,8 @@ const MarqueeText = () => {
     return (
         <div className="overflow-hidden w-full max-h-[40px] py-[1px] whitespace-nowrap bg-white ">
             <div className="inline-block w-full  marquee">
-                <span className="text-[#029046] font-bold">Cayad Auto Transport</span> - assists with all your auto
-                <span className="text-[#00a1ef] mx-2 font-bold">shipping</span> needs in all the United States.
+                <span className="text-[#029046] font-bold">Cayad Auto Transport</span> - is the most cost effective
+                <span className="text-[#00a1ef] mx-2 font-bold">5 stars auto</span> transport company.
             </div>
         </div>
     );
