d16a820047a31f015929344976c4beaee9c48d73
MiguelJMS <miguelmarabay19@gmail.com>
2024-07-15 12:28:05 -0400

    refactors

--- src/components/buttons/ButtonScrollTop.tsx
+++ src/components/buttons/ButtonScrollTop.tsx
@@ -25,30 +25,34 @@ const ButtonScrollTop = () => {
   }, []);
 
   return (
-    <div
-      className={`fixed right-[70px] ${isAtBottom ? 'bottom-[100px]' : 'bottom-[1.2rem]'} transition-all duration-300`}
-      style={{ display: isVisible ? 'block' : 'none' }}  // Show/hide based on scroll position
-    >
-      <button
-        onClick={scrollToTop}
-        className="w-9 h-9 bg-btn-blue text-white flex justify-center items-center rounded-full shadow-lg"
+    <div className="fixed bottom-4 right-20 flex flex-col items-end">
+
+      <div
+        className={`fixed right-[70px] ${isAtBottom ? 'bottom-[100px]' : 'bottom-[1.2rem]'} transition-all duration-300`}
+        style={{ display: isVisible ? 'block' : 'none' }}  // Show/hide based on scroll position
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="size-6 stroke-2"
+        <button
+          onClick={scrollToTop}
+          className="w-9 h-9 bg-btn-blue text-white flex justify-center items-center rounded-full shadow-lg"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18"
-          />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6 stroke-2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
+
   );
 };
 
