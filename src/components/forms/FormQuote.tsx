72342b1c50789efb2025728b026e6d9d32df0d5d
MiguelJMS <miguelmarabay19@gmail.com>
2024-07-18 12:35:23 -0400

    add new pages and edit vector

--- src/components/forms/FormQuote.tsx
+++ src/components/forms/FormQuote.tsx
@@ -261,7 +261,7 @@ const FormQuote = () => {
     const formattedDate = formatDate(data.ship_date);
 
     const dataToLead = {
-      AuthKey: "849d9659-34b5-49c5-befd-1cd238e7f9fc",
+      AuthKey: "f895aa95-10ea-41ae-984f-c123bf7e0ff0",
       ...data,
       ship_date: formattedDate,
     }
