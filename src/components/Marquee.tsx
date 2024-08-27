edabba682ca03f2288e753c8f9dc47e46968218f
MiguelJMS <miguelmarabay19@gmail.com>
2024-08-27 12:03:31 -0400

    fix accebility

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,14 +1,14 @@
 import '../styles/marquee.css'; // Asegúrate de tener el archivo CSS configurado correctamente
 
 const MarqueeText = () => {
-    return (
-        <div className="overflow-hidden w-full max-h-[40px] py-[1px] whitespace-nowrap bg-white ">
-            <div className="inline-block w-full  marquee">
-                <span className="text-[#029046] font-bold">Cayad Auto Transport</span> - is the most cost effective
-                <span className="text-[#00a1ef] mx-2 font-bold">5 stars </span>auto transport company.
-            </div>
-        </div>
-    );
+  return (
+    <div className="overflow-hidden w-full max-h-[40px] py-[1px] whitespace-nowrap text-gray-800 bg-white ">
+      <div className="inline-block w-full   marquee">
+        <span className="text-[#006b34] font-bold">Cayad Auto Transport</span> - is the most cost effective
+        <span className="text-[#00a1ef] mx-2 font-bold">5 stars </span>auto transport company.
+      </div>
+    </div>
+  );
 };
 
 export default MarqueeText;
