6f9ca7fea88ca71bffecc37aa86abb395babf4ee
AD Gomez <garyeld12@gmail.com>
2025-04-01 20:27:22 -0400

    updating seo intention

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,7 +57,7 @@ const whyUs = [
   //{ description: 'About us', href: '/why-us/about-us/', icon: CursorArrowRaysIcon },
   { description: 'Our team', href: '/why-us/our-team/', icon: FingerPrintIcon },
   { description: 'Vision and Mission', href: '/why-us/vision-mission/', icon: FingerPrintIcon },
-  { description: 'Blog', href: '#', icon: FingerPrintIcon }
+  { description: 'Blog 🚧', href: '#', icon: FingerPrintIcon }
 ]
 
 function classNames (...classes: any) {
