ea3436f4869731679daefa385ac78ba10eac6e3f
AD Gomez <garyeld12@gmail.com>
2025-04-17 21:24:11 -0400

    updating

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -8,7 +8,7 @@ const steps = [
   {
     label: 'Step 1',
   title: 'Quote and Book Your Auto Transport',
-  description: ' Enter your pickup and delivery info to get a free quote in seconds. Choose your transport type, confirm your price, and book online — fast, secure, and backed by a reliable car transportation company.',
+  description: ' Enter your pickup and delivery info to get a free quote in seconds. Choose your transport type, confirm your price, and book online fast, secure, and backed by a reliable car transportation company.',
   span: 'Tap or click to get your instant vehicle shipping quote',
     img: why1,
     hasButton: true
@@ -16,14 +16,14 @@ const steps = [
   {
     label: 'Step 2',
   title: 'We’ll Pick Up Your Car',
-  description: 'Our Driver meets you where it’s most convenient — even at home or work. It’s flexible, secure, and handled by trusted vehicle transport pros.',
+  description: 'Our Driver meets you where it’s most convenient, even at home or work. It’s flexible, secure, and handled by trusted vehicle transport pros.',
 
     img: why2
   },
   {
     label: 'Step 3',
   title: 'Receive Your Car',
-  description: 'The driver will call ahead as your car arrives. Meet them at your driveway or any agreed spot. Fast, reliable vehicle delivery — handled by trusted auto transport professionals.',
+  description: 'The driver will call ahead as your car arrives. Meet them at your driveway or any agreed spot. Fast, reliable vehicle delivery, handled by trusted auto transport professionals.',
 
     img: why3
   },
